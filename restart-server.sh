564d21e8d88a559add268216945aa826b8c08bd1
thebesttv <taojuntian@outlook.com>
2022-01-26 20:49:41 +0800

    add authbind for 80 port

--- restart-server.sh
+++ restart-server.sh
@@ -2,5 +2,6 @@
 
 emacsclient -s server -e '(kill-emacs)'
 pkill -f "emacs --daemon=server"
+# authbind emacs --daemon=server  # for 80 port
 emacs --daemon=server
 emacsclient -nw -s server -e '(load "~/eserver/main-server.el")' -n
